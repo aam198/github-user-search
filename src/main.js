4bdb4376c3cb120aca70ff5427b6f330ddad37f5
aam198 <aam198@psu.edu>
2021-10-07 15:51:24 -0400

    add: axios

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
-console.log(123)
+const APIURL = 'https://api.github.com/users/';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+
+// Function To GET username
+
+async function getUser(username){
+  try{
+    const { data } = await axios(APIURL + username);
+
+    console.log(data)
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const username = search.value;
+  if(username){
+    getUser(username)
+    //clear search value
+    search.value ='';
+  }
+})
